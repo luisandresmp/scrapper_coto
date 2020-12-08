92e5719506cd328a4b237ee79424eaf92a3bc9a9
Luis Andres <luisandresmartinezperaza@gmail.com>
2020-12-08 19:42:13 -0300

    first functional scraping

--- scrapper_coto.py
+++ scrapper_coto.py
@@ -7,22 +7,22 @@ import os
 import time
 from progress.bar import ChargingBar
 
-def obtener_categorias():
+def get_categories():
     url = 'https://www.cotodigital3.com.ar/sitios/cdigi/'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
     subcategorias = soup.find_all('div', attrs={'class':'g1'})
-    todas = [hola.h2.a.get('href') for hola in subcategorias]
-    todas2 = [f'https://www.cotodigital3.com.ar{cat}' for cat in todas]
-    return(todas2)
+    all = [i.h2.a.get('href') for i in subcategorias]
+    all_2 = [f'https://www.cotodigital3.com.ar{i}' for i in all]
+    return(all_2)
 
-def hojas_del_catalogo(entrada):
+def catalogue_leaves(entry):
 
-    lista=[]    
+    list_catalogue=[]    
 
     # CANTIDAD DE HOJAS DE UNA MISMA CATEGORIA (numero de paginas a scrapear)
-    fragmento = '?Nf=product.endDate%7CGTEQ+1.6037568E12%7C%7Cproduct.startDate%7CLTEQ+1.6037568E12&No=' 
-    url = f'{entrada}{fragmento}{24}'
+    fragment = '?Nf=product.endDate%7CGTEQ+1.6037568E12%7C%7Cproduct.startDate%7CLTEQ+1.6037568E12&No=' 
+    url = f'{entry}{fragment}{24}'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
     unt = soup.find('p', attrs={'class':'titleSearchResults'})
@@ -30,23 +30,23 @@ def hojas_del_catalogo(entrada):
     unt_c3 = math.ceil(int(unt_c2)/24)
 
     for i in range(0,24*unt_c3,24):
-        url = f'{entrada}{fragmento}{i}'
+        url = f'{entry}{fragment}{i}'
         response = requests.get(url)
         if response.status_code == 200:
-            lista.append(url)
+            list_catalogue.append(url)
         else:
             pass
             
-    return(lista) 
+    return(list_catalogue) 
 
-def productos_por_pagina(pag): #LISTA DE PRODUCTOS POR PAGINA
+def products_per_page(pag): #LISTA DE PRODUCTOS POR PAGINA
     response = requests.get(pag)
     soup = BeautifulSoup(response.text, 'lxml')
     prod = soup.find_all('div', attrs={'class':'product_info_container'})
-    todos_prod = [prod.a.get('href') for prod in prod]
-    todos_ahora = [('https://www.cotodigital3.com.ar'+prod).replace(' ', '')  for prod in todos_prod]
-    #print(todos_ahora)
-    return(todos_ahora)    
+    all_prod = [prod.a.get('href') for prod in prod]
+    all_now = [('https://www.cotodigital3.com.ar'+prod).replace(' ', '')  for prod in all_prod]
+
+    return(all_now)    
 
 def scrapper_coto(url):
     try:
@@ -60,11 +60,9 @@ def scrapper_coto(url):
             if name is None:
                 coto_product['name'] = None
             else:
-                #print(name)
                 name_text = name.get_text()
                 coto_product['name'] = " ".join(name_text.split())
-                #print('nombre')
-
+                
             # PRECIO DE PRODUCTO
             price = soup_response.find('span', attrs={'class':'atg_store_newPrice'})
             if price is None:
@@ -73,51 +71,41 @@ def scrapper_coto(url):
                 price_text = price.get_text()
                 price_clean = " ".join(price_text.split())
                 coto_product['price'] = price_clean[15:]
-                #print('precio')
-
+                
             # DESCUENTO DEL PRODUCTO
             discount = soup_response.find('span', attrs={'class':'text_price_discount'})
-            #print (discount)
             if discount is None:
                 coto_product['discount'] = None 
             else:
                 coto_product['discount'] = discount.get_text()
-                #print('descuento')
-
+            
             # PRECIO CON DESCUENTO
             discount_price = soup_response.find('span', attrs={'class':'price_discount'})
             if discount_price is None:
                 coto_product['discount_price'] = None  
-                #print('precio descuento')   
             else:
                 coto_product['discount_price'] = discount_price.get_text()
-                #print('precio descuento')    
-
+            
             #CUOTAS SIN INTERES
             fees = soup_response.find('div', attrs={'class':'product_discount_pay'})
             if fees is None:
                 coto_product['fees'] = None
-                #print('cuotas')      
             else:
                 fees_text = fees.get_text()
                 coto_product['fees'] = " ".join(fees_text.split()) 
-                #print('cuotas') 
-
+            
             # DATO POR UNIDAD
             unit = soup_response.find('span', attrs={'class':'unit'})
             unit_text = unit.get_text()
             if unit_text:
                 coto_product['unit'] = " ".join(unit_text.split()) 
-                #print('unidad')       
             else:
                 coto_product['unit'] = None
-                #print('unidad')
             
             # IMAGEN DEL PRODUCTO
             img = soup_response.find('a', attrs={'class':'gall-item'})
             if img:
                 coto_product['img'] = img.img.get('src')
-                #print('imagen')
             else:
                 coto_product['img'] = None
 
@@ -128,94 +116,99 @@ def scrapper_coto(url):
                 coto_product['url'] = None
 
             coto_product['download'] = datetime.now()    
-        #print(coto_product)    
         return coto_product        
             
     except Exception as e:
-        coto_product = {}
-        coto_product['url'] = url
-        #pass
-        return coto_product    
-
+        pass
+        
 def load_data(name,data_clean):
 
-    if os.path.isfile ('db_coto.csv'):
-        print('loading data....\n')
-        data_clean.to_csv('db_coto.csv', mode='a', header=False)
+    date = datetime.now().strftime('%Y_%m_%d')
+    file_name = '{name}_{datetime}.csv'.format(name=name, datetime=date)  
+
+    if os.path.isfile (f'{file_name}'):
+        print(f'loading data in {file_name}....\n' )       
+        data_clean.to_csv(file_name, mode='a', header=False)
         print('COMPLETED')
     else:
-        print(f'\nCreate db_coto.csv....\n')
-        data_clean.to_csv('db_coto.csv', encoding='utf-8')
+        print(f'\nCreate {file_name}....\n')
+        data_clean.to_csv(file_name, encoding='utf-8')
         print('COMPLETED')
 
+def scrapper_strong(option,url):
+    print(f'\n\n Starting download of {url}')
 
-def categoria_hard_delay(url):
-    print(f'Iniciando descarga de {url}')
-    #prueba= 'https://www.cotodigital3.com.ar/sitios/cdigi/browse/catalogo-limpieza-lavandina/_/N-cliwvx'
+    if option == 0:
+        option = 'strong_coto'
+    else:
+        option = 'coto'
+
     coto = []
     producto_coto=[]
     error = []
 
-    toda_categoria = hojas_del_catalogo(url)
+    toda_categoria = catalogue_leaves(url)
     if len(toda_categoria)>0:
-        print(f'se encontraron {len(toda_categoria)} paginas')
+        print(f'{len(toda_categoria)} pages')
         for i in toda_categoria:
-            prod = productos_por_pagina(i)
+            prod = products_per_page(i)
             coto+=prod
     
-    print(f'se encontraron {len(coto)} productos')
-    print(f'\nInicio del proceso: {datetime.now()}\n')
+    print(f'{len(coto)} products')
+    print(f'\n Start of the process: {datetime.now()}\n')
     start = datetime.now()
     bar = ChargingBar('Scrapeando:', max=len(coto))
     for i in coto:
         data = scrapper_coto(i)
-        producto_coto.append(data)
-        if len(data) == 1:
-           error.append(data) 
-        else:   
-            pass
+        if data:        
+            if len(data) == 1:
+                error.append(data) 
+            else:
+                producto_coto.append(data)
         bar.next()
     
     bar.finish() 
 
-    #print(len(toda_categoria))
     df = pd.DataFrame(producto_coto)
-    load_data(url,df)
-    #print(len(coto))
-    print(f'se encontraron {len(error)} errores')
+    load_data(option,df)
+    if len(error) > 0:
+        error_name = 'Error_coto'
+        df_error = pd.DataFrame(error)
+        load_data(error_name,df_error)
+    
+
+    print(f'{len(error)} errors found')
     print(f'{datetime.now()}')
     end = datetime.now()
-    print(f'\nFin del proceso: {end - start}\n')
-    print(f'Esperando 2 minutos ....\n')
-    time.sleep(120)
+    print(f'\nEnd of the process: {end - start}\n')   
 
-def validar():
-    var_onu = int(input('\n Desea hacer un: \n\n 0 .-HARD scrapper coto \n 1 .-HIGH scrapper coto \n 2 .-Exit \n\n --->> '))
-    while var_onu > 1:
-        if var_onu == 2:
+def validate():
+    var_enter = int(input('\n What you want to do? \n\n 0 .-STRONG scrapper coto \n 1 .-CATEGORY scrapper coto \n 2 .-Exit \n\n Only option number --->> '))
+    while var_enter > 1:
+        if var_enter == 2:
             print('\n Bye bye')
             break
-        print('\n Error, debe introducir el numero de una opcion valida')
-        var_onu = int(input('\n --->> '))
+        print('\n Error, enter the number of a valid option')
+        var_enter = int(input('\n --->> '))
 
-    return var_onu    
-
-
+    return var_enter    
 
 def run():
-    #categorias = obtener_categorias()
-    
-    hola = validar()
-    print(hola)
-    
+    option = validate()
 
-    #print(categorias[83:])
-    #for i in categorias[83:]:
-    #    categoria_hard_delay(i)
- 
-    #quepaso = scrapper_coto('https://www.cotodigital3.com.ar/sitios/cdigi/producto/-lavandina-en-gel-vim-original-700-ml/_/A-00478108-00478108-200')     
-    #print(quepaso)  
+    if option == 0:
+        categorias = get_categories()
+        print(f'\n {len(categorias)} categories found\n')
+        for i, category in enumerate(categorias):
+            print(f'Scrapeando category {i}/{len(categorias)}')
+            scrapper_strong(option,category)
+            print(f'Waiting 2 minutes ....\n')
+            time.sleep(120)           
+    if option == 1:
+        category = input('\n\n Enter the category link \n\n (Example: "https://www.cotodigital3.com.ar/sitios/cdigi/browse/catalogo-limpieza-lavandina/_/N-cliwvx")\n\n --->>')
+        scrapper_strong(option,category)
+    else:
+        pass
      
-   
 if __name__ == "__main__":
     run()
